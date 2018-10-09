87248ccd882d5cd4cacfe21a983a466e575dd6e5
djbios <dorandval@gmail.com>
2018-10-09 15:55:50 +0300

    Gitignore

--- svh/svh/settings.py
+++ svh/svh/settings.py
@@ -75,9 +75,11 @@ WSGI_APPLICATION = 'svh.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'qw',
+        'USER': 'qw',
+        'HOST': 'postgres',
+    },
 }
 
 
@@ -118,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+from svh.settings_local import *
