84f84cd21d30d70fc781f03e47cd67d79a1ad236
andrey dorofeev <andrey.dorofeev@renhealth.com>
2019-07-23 17:08:03 +0300

    deploy

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -6,5 +6,7 @@
 # sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 # sudo chmod +x /usr/local/bin/docker-compose
 # git clone https://github.com/djbios/SVH
+cd "$(dirname "$0")"
+
 docker-compose -p svh build --pull
 docker-compose -p svh up -d --remove-orphans
