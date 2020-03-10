fc2b6fbf2b9663a25bf0c0dcbfbd8e69175f4f52
djbios <dorandval@gmail.com>
2020-03-10 10:07:06 +0300

    append frontend , cleanup

---
+++ frontend/src/components/SvhHeader.vue
@@ -0,0 +1,14 @@
+<template>
+  <nav class="navbar navbar-light">
+  </nav>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  name: "SvhHeader",
+  computed: {
+  }
+};
+</script>
---
+++ frontend/src/components/SvhTagsBar.vue
@@ -0,0 +1,22 @@
+<template>
+   
+</template>
+
+<script>
+    export default {
+        name: "SvhTagsBar",
+        vars: {},
+        data () {
+        },
+        mounted() {
+            axios
+                .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+                .then(response => (this.js = response.data.bpi.USD));
+        }
+    }
+
+</script>
+
+<style scoped>
+
+</style>
